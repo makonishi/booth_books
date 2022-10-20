776c28c872699b2050aa31665bfaf5d9ab811d8c
Makotox0207 <xiaoxishicheng@gmail.com>
2022-10-20 23:26:02 +0900

    `async/await`でリファクタリング

--- 04-async/get-user-then.js
+++ 04-async/get-user-then.js
@@ -1,24 +1,24 @@
 import fetch from "node-fetch";
 
-const getUser = (userID) => 
-  fetch(`https://jsonplaceholder.typicode.com/users/${userID}`)
-    .then((response) => {
-      if(!response.ok) {
-        throw new Error(`${response.status} Error`);
-      } else {
-        return response.json();
-      }
-    })
+const getUser = async (userID) => {
+  const response = await fetch(`https://jsonplaceholder.typicode.com/users/${userID}`);
+
+  if(!response.ok) throw new Error(`${response.status} Error`);
+
+  return response.json();
+};
 
 console.log('--- Start ---');
 
-getUser(2)
-.then((user) => {
-  console.log(user);
-})
-.catch((error) => {
-  console.log(error);
-})
-.finally(() => {
-  console.log('--- Completed ---');
-});
+const main = async () => {
+  try {
+    const user = await getUser(2);
+    console.log(user);
+  } catch (error) {
+    console.log(error);
+  } finally {
+    console.log('--- Completed ---');
+  }
+};
+
+main();
